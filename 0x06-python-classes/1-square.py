ebea3a0fd274ef2075d680ea973a80a14c0de6be
Fedy chihaoui <84166701+fedy69@users.noreply.github.com>
2022-02-13 15:23:15 +0100

    Delete 1-square.py

--- 0x06-python-classes/1-square.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-class Square:
-    """Square with private instance attribute"""
-    def __init__(self, size):
-        """Initialize data"""
-        self.__size = size
-        """Private instantiation of size"""
