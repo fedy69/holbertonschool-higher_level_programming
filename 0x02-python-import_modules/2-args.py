935cbc5aaac725760fecbe8a2dec77ec25040f14
Fedy chihaoui <84166701+fedy69@users.noreply.github.com>
2022-06-25 02:48:03 +0100

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-args_number = len(sys.argv) - 1
-if args_number <= 1:
-    if args_number == 1:
-        print("1 argument:")
-        print("{}: {}".format(1, sys.argv[1]))
+    length = len(sys.argv) - 1
+    if length == 0:
+        print('0 arguments.')
+    elif length == 1:
+        print('1 argument:')
+        print('1: {:s}'.format(sys.argv[1]))
     else:
-        print("0 argument.")
-else:
-    print("{} arguments:".format(args_number))
-    for i in range(1, len(sys.argv)):
-        print("{}: {}".format(i, sys.argv[i]))
+        print("{:d} arguments:".format(length))
+        for i in range(length):
+            print('{:d}: {:s}'.format(i + 1, sys.argv[i + 1]))
