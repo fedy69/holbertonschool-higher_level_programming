5fe43d54f1fef3825aad4626bdecfb6cf4e8f9cf
Fedy chihaoui <sasouciouchiha@gmail.com>
2022-07-11 15:37:22 +0100

    size of body of response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Takes in URL, sends request to URL, displays size of body of response
-curl -sI "$1" | grep -i Content-Length | cut -d ' ' -f2
+curl -w '%{size_download}\n' -so /dev/null "$1"
