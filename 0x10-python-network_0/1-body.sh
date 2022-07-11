81a71f7f3c1c77c83077cbc785351783300463d2
Fedy chihaoui <sasouciouchiha@gmail.com>
2022-07-11 15:33:03 +0100

    script that takes in a URL

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#Takes in URL, sends request to URL, displays body of response
-curl -s -L -X GET "$1"
+# Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
+curl -sLX GET "$1"
