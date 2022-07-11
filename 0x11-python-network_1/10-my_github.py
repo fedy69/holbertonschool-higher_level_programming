4601d0b0c49fabb81b47423d2099839d6a1c7cf4
Fedy chihaoui <sasouciouchiha@gmail.com>
2022-07-11 16:34:14 +0100

    fix

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -4,17 +4,11 @@ List 10 commits starting with most recent of repo
 rails by user rails
 """
 
-import requests
 import sys
+import requests
+from requests.auth import HTTPBasicAuth
 
-
-if __name__ == '__main__':
-    owner = sys.argv[2]
-    repository = sys.argv[1]
-    url = 'https://api.github.com/repos/{}/{}/commits'.format(owner,
-                                                              repository)
-    request = requests.get(url)
-    json_response = request.json()
-    for i in json_response[:10]:
-        print('{}: {}'.format(i.get('sha'),
-                              i.get('commit').get('author').get('name')))
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
