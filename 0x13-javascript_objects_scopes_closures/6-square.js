fbeae1d7a9cc4456b76132b7cce53170fa387696
Fedy chihaoui <84166701+fedy69@users.noreply.github.com>
2022-07-15 00:59:24 +0100

    Update 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,16 +1,18 @@
 #!/usr/bin/node
 // class  that defines a square and inherits from Rectangle of 4-rectangle.js
-module.exports = class Square extends require('./5-square.js') {
-  charPrint (c) {
-    let prit;
 
-    if (c) {
-      prit = ('' + c.repeat(this.width) + '\n').repeat(this.height - 1);
-      prit += ('' + c.repeat(this.width));
-    } else {
-      prit = ('X'.repeat(this.width) + '\n').repeat(this.height - 1);
-      prit += ('X'.repeat(this.width));
-    }
-    console.log(prit);
+const OldSquare = require('./5-square');
+
+module.exports = class Square extends OldSquare {
+  constructor (size) {
+    super(size, size);
+  }
+
+  double () {
+    super.double();
+  }
+
+  charPrint (c = 'X') {
+    super.print(c);
   }
 };
