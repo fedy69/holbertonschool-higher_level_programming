791de74ba3a9fbfea1916d6d969ddc61dbe2bd75
fedy69 <sasouciouchiha@gmail.com>
2022-07-14 16:15:19 -0700

    js tasks

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,21 +1,26 @@
 #!/usr/bin/node
-// script that prints all characters of a Star Wars movie:
+// script that prints all characters of a Star Wars
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request.get(url, function (err, res, body) {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  const characters = JSON.parse(body).characters;
-  for (let i = 0; i < characters.length; i++) {
-    request.get(characters[i], function (err, res, body) {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      const name = JSON.parse(body).name;
-      console.log(name);
-    });
-  }
-});
+
+const episodeId = process.argv[2];
+
+if (parseInt(episodeId) < 8) {
+  const url = 'https://swapi-api.hbtn.io/api/films/' + episodeId;
+
+  request(url, (err, res, body) => {
+    if (err) {
+      return console.log(err);
+    }
+    const { characters } = JSON.parse(body);
+
+    characters.forEach(
+      (url, index) => {
+        request(url, (err, res, body) => {
+          if (err) {
+            return console.log(err);
+          }
+          console.log(JSON.parse(body).name);
+        });
+      });
+  });
+}
