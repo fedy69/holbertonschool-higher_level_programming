9a24a461c39f488d99f239a1287a5fc927ba466c
Fedy chihaoui <84166701+fedy69@users.noreply.github.com>
2022-07-15 00:22:36 +0100

    Update 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,26 +1,30 @@
 #!/usr/bin/node
-
 const request = require('request');
+const url = 'http://swapi.co/api/films/';
+let id = parseInt(process.argv[2], 10);
+let characters = [];
 
-// prints all characters of a Star Wars movie:
-
-function helpRequest (arr, i) {
-  if (i === arr.length) {
-    return;
-  }
-  request(arr[i], function (error, response, body) {
-    if (error) {
-      console.error(error);
+request(url, function (err, response, body) {
+  if (err == null) {
+    const resp = JSON.parse(body);
+    const results = resp.results;
+    if (id < 4) {
+      id += 3;
+    } else {
+      id -= 3;
     }
-    console.log(JSON.parse(body).name);
-    helpRequest(arr, i + 1);
-  });
-}
-
-request('http://swapi.co/api/films/' + process.argv[2], function (error, response, body) {
-  if (error) {
-    console.error(error);
+    for (let i = 0; i < results.length; i++) {
+      if (results[i].episode_id === id) {
+        characters = results[i].characters;
+        break;
+      }
+    }
+    for (let j = 0; j < characters.length; j++) {
+      request(characters[j], function (err, response, body) {
+        if (err == null) {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    }
   }
-  const charac = JSON.parse(body).characters;
-  helpRequest(charac, 0);
 });
