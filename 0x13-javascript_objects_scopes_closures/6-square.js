4e9a812e386c04ec03a034df5bc66e617748312d
Fedy chihaoui <84166701+fedy69@users.noreply.github.com>
2022-07-15 02:26:45 +0100

    Update 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,18 +1,30 @@
 #!/usr/bin/node
 // class  that defines a square and inherits from Rectangle of 4-rectangle.js
 
-const OldSquare = require('./5-square');
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
-module.exports = class Square extends OldSquare {
-  constructor (size) {
-    super(size, size);
-  }
 
-  double () {
-    super.double();
-  }
+class User(BaseModel, Base):
+    """Representation of a user """
+    if models.storage_t == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
 
-  charPrint (c = 'X') {
-    super.print(c);
-  }
-};
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        super().__init__(*args, **kwargs)
