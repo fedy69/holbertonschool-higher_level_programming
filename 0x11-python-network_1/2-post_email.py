72b145e67920220a13b5841937b6c57d607a9660
Fedy chihaoui <84166701+fedy69@users.noreply.github.com>
2022-07-15 03:31:43 +0100

    Update 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     url = argv[1]
     email = argv[2]
     data = parse.urlencode({'email': email}).encode()
-    # this will make the method "POST"
+    # it will make the method "POST"
     req = request.Request(url=url, data=data)
 
     with request.urlopen(req) as f:
