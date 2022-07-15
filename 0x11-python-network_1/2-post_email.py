5320cd15d96e089cb578c217fe1050ad711ae73e
Fedy chihaoui <84166701+fedy69@users.noreply.github.com>
2022-07-15 03:30:20 +0100

    Update 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,35 +1,19 @@
-
 #!/usr/bin/python3
 """
-function that prints  the State object with the name passed as argument
-from the database hbtn_0e_6_usa using ORM.
-"""
+sends a POST request to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)"""
+
+from urllib import request, parse
+from sys import argv
 
 
 if __name__ == "__main__":
-    import sqlalchemy
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
-    from model_state import Base, State
-    from sys import argv, exit
 
-    if len(argv) != 5:
-        print("Usage: ./7.py <username> <password> <database> <name>")
-        exit(1)
+    url = argv[1]
+    email = argv[2]
+    data = parse.urlencode({'email': email}).encode()
+    # this will make the method "POST"
+    req = request.Request(url=url, data=data)
 
-    user = argv[1]
-    passwd = argv[2]
-    database = argv[3]
-    name = argv[4]
-
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-
-    session = Session(engine)
-    state = session.query(State).filter_by(name=argv[4]).first()
-    if state:
-        print(state.id)
-    else:
-        print("Not found")
-    session.close()
+    with request.urlopen(req) as f:
+        print(f.read().decode('utf-8'))
